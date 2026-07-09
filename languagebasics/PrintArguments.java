9c610aff4fef312b2f72585533c1edb61dab35e6
kalpanavajrala746-netizen <kalpanavajrala746@gmail.com>
2026-07-09 17:43:25 +0530

    Add files via upload

---
+++ languagebasics/PrintArguments.java
@@ -0,0 +1,23 @@
+package languagebasics;
+
+public class PrintArguments {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+
+            System.out.println("No Values");
+
+        } else {
+
+            for (int i = 0; i < args.length; i++) {
+
+                System.out.print(args[i]);
+
+                if (i != args.length - 1) {
+                    System.out.print(",");
+                }
+            }
+        } // Closed else block
+    } // Closed main method
+} // Closed class
