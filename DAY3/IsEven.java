0f86fe93a84f76b59ebf81a4bc324795c39878b1
kalpanavajrala746-netizen <kalpanavajrala746@gmail.com>
2026-07-16 16:50:10 +0530

    Add files via upload

---
+++ DAY3/IsEven.java
@@ -0,0 +1,23 @@
+package DAY3;
+
+import java.util.Scanner;
+
+public class IsEven {
+
+    static int isEven(int n) {
+        if (n % 2 == 0) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(isEven(n));
+
+        sc.close();
+    }
+}
